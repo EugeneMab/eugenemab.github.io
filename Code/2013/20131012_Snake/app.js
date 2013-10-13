d6801bd5d5e7a801830d0be1e98bb3846562c8ee
Eugene Mab <jianhuangysword@msn.com>
2013-10-13 13:57:09 -0700

    Snake complete

--- Code/2013/20131012_Snake/app.js
+++ Code/2013/20131012_Snake/app.js
@@ -1,73 +1,330 @@
 var Snake;
 (function (Snake) {
-    var Cell = (function () {
-        function Cell(row, col, td) {
-            this.row = row;
-            this.col = col;
-            this.td = td;
-            var self = this;
-            this.td.onclick = function () {
-                Snake.Ctrl.onClickCell(self.row, self.col);
-            };
-            this.td.className = "cellNone" + ((row + col) % 2);
+    var Config = (function () {
+        function Config() {
+            this.height = 30;
+            this.width = 40;
+            this.timerMillisec = 100;
+            this.scoreForMove = 1;
+            this.scoreForBonus = 1000;
+            this.growthForBonus = 2;
         }
-        return Cell;
+        return Config;
     })();
-    var Board = (function () {
-        function Board(contentDiv, width, height) {
-            this.width = width;
-            this.height = height;
+    var config = new Config();
+
+    var Vector = (function () {
+        function Vector(y, x) {
+            this.y = y;
+            this.x = x;
+        }
+        Vector.prototype.add = function (delta) {
+            return new Vector(this.y + delta.y, this.x + delta.x);
+        };
+        return Vector;
+    })();
+    Snake.Vector = Vector;
+    var CellState;
+    (function (CellState) {
+        CellState[CellState["none"] = 0] = "none";
+        CellState[CellState["head"] = 1] = "head";
+        CellState[CellState["cursor"] = 2] = "cursor";
+        CellState[CellState["bonus"] = 3] = "bonus";
+        CellState[CellState["dead"] = 4] = "dead";
+    })(CellState || (CellState = {}));
+    var CellModel = (function () {
+        function CellModel(vec, state) {
+            this.vec = vec;
+            this.state = state;
+        }
+        return CellModel;
+    })();
+    var Direction;
+    (function (Direction) {
+        Direction[Direction["right"] = 0] = "right";
+        Direction[Direction["down"] = 1] = "down";
+        Direction[Direction["left"] = 2] = "left";
+        Direction[Direction["up"] = 3] = "up";
+    })(Direction || (Direction = {}));
+    var directionVectors = [
+        new Vector(0, 1),
+        new Vector(1, 0),
+        new Vector(0, -1),
+        new Vector(-1, 0)
+    ];
+
+    var Model = (function () {
+        function Model() {
+        }
+        Model.prototype.reset = function (context, dim) {
+            this.context = context;
+            this.gameOver = false;
+            this.score = 0;
+            this.dim = dim;
+            this.board = new Array(this.dim.y);
+            for (var y = 0; y < dim.y; y++) {
+                this.board[y] = new Array(this.dim.x);
+                for (var x = 0; x < dim.x; x++) {
+                    this.board[y][x] = new CellModel(new Vector(y, x), CellState.none);
+                }
+            }
+            var center = new Vector(Math.floor(this.dim.y / 2), Math.floor(this.dim.x / 2));
+            this.cursors = new Array(center);
+            this.direction = Direction.right;
+            this.getCell(center).state = CellState.head;
+            this.growth = 0;
+            this.createBonus();
+        };
+        Model.prototype.getCell = function (vector) {
+            return this.board[vector.y][vector.x];
+        };
+        Model.prototype.createBonus = function () {
+            for (; ;) {
+                var vec = new Vector(Math.floor(Math.random() * this.dim.y), Math.floor(Math.random() * this.dim.x));
+                var cell = this.getCell(vec);
+                if (cell.state != CellState.none) {
+                    continue;
+                }
+                cell.state = CellState.bonus;
+                this.context.updateCell(vec);
+                return;
+            }
+        };
+        Model.prototype.move = function () {
+            this.score += config.scoreForMove;
+            this.context.updateScore();
+
+            var head = this.cursors[0];
+            this.getCell(head).state = CellState.cursor;
+            this.context.updateCell(head);
+
+            var next = this.wrapAround(head.add(directionVectors[this.direction]));
+            var nextCell = this.getCell(next);
+            switch (nextCell.state) {
+                case CellState.none: {
+                    this.cursors.splice(0, 0, next);
+
+                    nextCell.state = CellState.head;
+                    this.context.updateCell(next);
+                    break;
+                }
+                case CellState.head: {
+                    nextCell.state = CellState.dead;
+                    this.context.updateCell(next);
+
+                    this.gameOver = true;
+                    this.context.updateGameover();
+                    break;
+                }
+                case CellState.cursor: {
+                    nextCell.state = CellState.dead;
+                    this.context.updateCell(next);
+
+                    this.gameOver = true;
+                    this.context.updateGameover();
+                    break;
+                }
+                case CellState.bonus: {
+                    this.score += config.scoreForBonus;
+                    this.context.updateScore();
+
+                    nextCell.state = CellState.cursor;
+                    this.context.updateCell(next);
+
+                    this.growth += config.growthForBonus;
+                    this.cursors.splice(0, 0, next);
+                    this.createBonus();
+                    break;
+                }
+            }
+            if (this.growth != 0) {
+                this.growth--;
+                return;
+            }
+            var last = this.cursors[this.cursors.length - 1];
+            this.cursors.splice(this.cursors.length - 1, 1);
+            this.getCell(last).state = CellState.none;
+            this.context.updateCell(last);
+        };
+        Model.prototype.wrapAround = function (input) {
+            var y = input.y < 0 ? this.dim.y - 1 : input.y >= this.dim.y ? 0 : input.y;
+            var x = input.x < 0 ? this.dim.x - 1 : input.x >= this.dim.x ? 0 : input.x;
+            return new Vector(y, x);
+        };
+        Model.prototype.turn = function (dir) {
+            if (dir == this.direction) {
+                return;
+            }
+            if (this.getCell(this.wrapAround(this.cursors[0].add(directionVectors[dir]))).state == CellState.cursor) {
+                return;
+            }
+            this.direction = dir;
+        };
+        return Model;
+    })();
+
+    var CellView = (function () {
+        function CellView(cellModel, td) {
+            this.cellModel = cellModel;
+            this.td = td;
+            this.oddSuffix = (this.cellModel.vec.y + this.cellModel.vec.x) % 2 == 0 ? "0" : "1";
+            var that = this;
+            this.td.onclick = function () {
+                Snake.ctrl.onClickCell(that.cellModel.vec);
+            };
+            this.td.className = "cell_common cell_" + CellState[this.cellModel.state] + "_" + this.oddSuffix;
+        }
+        CellView.prototype.refresh = function () {
+            this.td.className = "cell_common cell_" + CellState[this.cellModel.state] + "_" + this.oddSuffix;
+        };
+        return CellView;
+    })();
+    var View = (function () {
+        function View() {
+        }
+        View.prototype.reset = function (model) {
+            this.model = model;
+            this.docElem = document;
+            this.contentDiv = this.docElem.getElementById("content");
+            this.scoreDiv = this.docElem.getElementById("score");
+            this.gameoverDiv = this.docElem.getElementById("gameover");
             var contentHtml = "";
             contentHtml += "<table border='0' cellpadding='0' cellspacing='0'>";
-            for (var row = 0; row < height; row++) {
+            for (var y = 0; y < this.model.dim.y; y++) {
                 contentHtml += "<tr>";
-                for (var col = 0; col < width; col++) {
-                    contentHtml += "<td id=cell_" + row + "_" + col + "></td>";
+                for (var x = 0; x < this.model.dim.x; x++) {
+                    contentHtml += "<td id=cell_" + y + "_" + x + "></td>";
                 }
                 contentHtml += "</tr>";
             }
             contentHtml += "</table>";
-            contentDiv.innerHTML = contentHtml;
-            this.cells = [];
-            for (var row = 0; row < height; row++) {
-                var line = [];
-                this.cells.push(line);
-                for (var col = 0; col < width; col++) {
-                    var td = document.getElementById("cell_" + row + "_" + col);
-                    line.push(new Cell(row, col, td));
+            this.contentDiv.innerHTML = contentHtml;
+            this.board = new Array(this.model.dim.y);
+            for (var y = 0; y < this.model.dim.y; y++) {
+                this.board[y] = new Array(this.model.dim.x);
+                for (var x = 0; x < this.model.dim.x; x++) {
+                    var td = this.docElem.getElementById("cell_" + y + "_" + x);
+                    this.board[y][x] = new CellView(this.model.board[y][x], td);
                 }
-                contentHtml += "</tr>";
             }
-        }
-        return Board;
+            this.scoreDiv.innerHTML = "Score: " + this.model.score;
+            this.gameoverDiv.innerHTML = "";
+        };
+        View.prototype.updateCell = function (vector) {
+            if (this.board == null) {
+                return;
+            }
+            this.getCell(vector).refresh();
+        };
+        View.prototype.getCell = function (vector) {
+            return this.board[vector.y][vector.x];
+        };
+        View.prototype.updateScore = function () {
+            this.scoreDiv.innerHTML = "Score: " + this.model.score;
+        };
+        View.prototype.updateGameover = function () {
+            if (this.model.gameOver) {
+                this.gameoverDiv.innerHTML = "Game Over!";
+            }
+        };
+        return View;
     })();
-    (function (Program) {
-        var contentDiv;
-        var width = 40;
-        var height = 30;
-        var board;
-        function reset() {
-            contentDiv = document.getElementById("content");
-            board = new Board(contentDiv, width, height);
-        }
-        Program.reset = reset;
-    })(Snake.Program || (Snake.Program = {}));
-    var Program = Snake.Program;
-    (function (Ctrl) {
-        var Program = Snake.Program;
-        function onLoad() {
-            Program.reset();
-        }
-        Ctrl.onLoad = onLoad;
-        function onClickCell(row, col) {
-            alert("clicked " + row + " " + col);
-        }
-        Ctrl.onClickCell = onClickCell;
-    })(Snake.Ctrl || (Snake.Ctrl = {}));
-    var Ctrl = Snake.Ctrl;
-})(Snake || (Snake = {}));
 
-window.onload = function () {
-    Snake.Ctrl.onLoad();
-};
+    var KeyCode;
+    (function (KeyCode) {
+        KeyCode[KeyCode["up"] = 38] = "up";
+        KeyCode[KeyCode["down"] = 40] = "down";
+        KeyCode[KeyCode["left"] = 37] = "left";
+        KeyCode[KeyCode["right"] = 39] = "right";
+        KeyCode[KeyCode["enter"] = 13] = "enter";
+    })(KeyCode || (KeyCode = {}));
+    var Ctrl = (function () {
+        function Ctrl() {
+            this.timeoutHandle = null;
+            var that = this;
+            window.onload = function () {
+                that.onLoad();
+            };
+        }
+        Ctrl.prototype.onLoad = function () {
+            var that = this;
+            window.onkeydown = function (ev) {
+                that.onKeyDown(ev);
+            };
+            this.onClickRestart();
+        };
+        Ctrl.prototype.onClickRestart = function () {
+            this.model = new Model();
+            this.view = new View();
+            this.model.reset(this.view, new Vector(config.height, config.width));
+            this.view.reset(this.model);
+            this.startTimer();
+        };
+        Ctrl.prototype.startTimer = function () {
+            this.stopTimer();
+            var that = this;
+            this.timeoutHandle = setTimeout(function () {
+                that.onTimeout();
+            }, config.timerMillisec);
+        };
+        Ctrl.prototype.stopTimer = function () {
+            if (this.timeoutHandle == null) {
+                return;
+            }
+            clearTimeout(this.timeoutHandle);
+            this.timeoutHandle = null;
+        };
+        Ctrl.prototype.onClickCell = function (vec) {
+            var y = vec.y / this.model.dim.y;
+            var x = vec.x / this.model.dim.x;
+            if (y + x < 1) {
+                if (y - x <= 0) {
+                    this.model.turn(Direction.up);
+                } else {
+                    this.model.turn(Direction.left);
+                }
+            } else {
+                if (y - x <= 0) {
+                    this.model.turn(Direction.right);
+                } else {
+                    this.model.turn(Direction.down);
+                }
+            }
+        };
+        Ctrl.prototype.onTimeout = function () {
+            if (this.model.gameOver) {
+                return;
+            }
+            this.startTimer();
+            this.model.move();
+        };
+        Ctrl.prototype.onKeyDown = function (ev) {
+            switch (ev.keyCode) {
+                case KeyCode.up: {
+                    this.model.turn(Direction.up);
+                    break;
+                }
+                case KeyCode.down: {
+                    this.model.turn(Direction.down);
+                    break;
+                }
+                case KeyCode.left: {
+                    this.model.turn(Direction.left);
+                    break;
+                }
+                case KeyCode.right: {
+                    this.model.turn(Direction.right);
+                    break;
+                }
+                case KeyCode.enter: {
+                    this.onClickRestart();
+                    break;
+                }
+            }
+        };
+        return Ctrl;
+    })();
+    Snake.Ctrl = Ctrl;
+    Snake.ctrl = new Ctrl();
+})(Snake || (Snake = {}));
 //# sourceMappingURL=app.js.map
--- Code/2013/20131012_Snake/app.ts
+++ Code/2013/20131012_Snake/app.ts
@@ -1,64 +1,319 @@
 module Snake {
-    class Cell {
-        constructor(public row: number, public col: number, public td: HTMLTableDataCellElement) {
-            var self: Cell = this;
-            this.td.onclick = () => {
-                Snake.Ctrl.onClickCell(self.row, self.col);
-            };
-            this.td.className = "cellNone" + ((row + col) % 2);
+    class Config {
+        height: number = 30;
+        width: number = 40;
+        timerMillisec: number = 100;
+        scoreForMove: number = 1;
+        scoreForBonus: number = 1000;
+        growthForBonus: number = 2;
+    }
+    var config = new Config();
+
+    export class Vector {
+        constructor(public y: number, public x: number) {
+        }
+        add(delta: Vector): Vector {
+            return new Vector(this.y + delta.y, this.x + delta.x);
         }
     }
-    class Board {
-        width: number;
-        height: number;
-        cells: Cell[][];
-        constructor(contentDiv: HTMLDivElement, width: number, height: number) {
-            this.width = width;
-            this.height = height;
+    enum CellState {
+        none,
+        head,
+        cursor,
+        bonus,
+        dead,
+    }
+    class CellModel {
+        constructor(public vec: Vector, public state: CellState) {
+        }
+    }
+    enum Direction {
+        right,
+        down,
+        left,
+        up,
+    }
+    var directionVectors: Vector[] = [
+        new Vector(0, 1),
+        new Vector(1, 0),
+        new Vector(0, -1),
+        new Vector(-1, 0),
+    ];
+    interface IModelContext {
+        updateCell(vector: Vector);
+        updateScore();
+        updateGameover();
+    }
+    class Model {
+        context: IModelContext;
+        gameOver: boolean;
+        score: number;
+        dim: Vector;
+        board: CellModel[][];
+        private cursors: Vector[];
+        private direction: Direction;
+        private growth: number;
+        reset(context: IModelContext, dim: Vector) {
+            this.context = context;
+            this.gameOver = false;
+            this.score = 0;
+            this.dim = dim;
+            this.board = new Array<CellModel[]>(this.dim.y);
+            for (var y: number = 0; y < dim.y; y++) {
+                this.board[y] = new Array<CellModel>(this.dim.x);
+                for (var x: number = 0; x < dim.x; x++) {
+                    this.board[y][x] = new CellModel(new Vector(y, x), CellState.none);
+                }
+            }
+            var center: Vector = new Vector(Math.floor(this.dim.y / 2), Math.floor(this.dim.x / 2));
+            this.cursors = new Array<Vector>(center);
+            this.direction = Direction.right;
+            this.getCell(center).state = CellState.head;
+            this.growth = 0;
+            this.createBonus();
+        }
+        private getCell(vector: Vector): CellModel {
+            return this.board[vector.y][vector.x];
+        }
+        private createBonus() {
+            for (; ;) {
+                var vec: Vector = new Vector(Math.floor(Math.random() * this.dim.y), Math.floor(Math.random() * this.dim.x));
+                var cell: CellModel = this.getCell(vec);
+                if (cell.state != CellState.none) {
+                    continue;
+                }
+                cell.state = CellState.bonus;
+                this.context.updateCell(vec);
+                return;
+            }
+        }
+        move() {
+            this.score += config.scoreForMove;
+            this.context.updateScore();
+
+            var head: Vector = this.cursors[0];
+            this.getCell(head).state = CellState.cursor;
+            this.context.updateCell(head);
+
+            var next: Vector = this.wrapAround(head.add(directionVectors[this.direction]));
+            var nextCell: CellModel = this.getCell(next);
+            switch (nextCell.state) {
+                case CellState.none: {
+                    this.cursors.splice(0, 0, next);
+
+                    nextCell.state = CellState.head;
+                    this.context.updateCell(next);
+                    break;
+                }
+                case CellState.head: {
+                    nextCell.state = CellState.dead;
+                    this.context.updateCell(next);
+
+                    this.gameOver = true;
+                    this.context.updateGameover();
+                    break;
+                }
+                case CellState.cursor: {
+                    nextCell.state = CellState.dead;
+                    this.context.updateCell(next);
+
+                    this.gameOver = true;
+                    this.context.updateGameover();
+                    break;
+                }
+                case CellState.bonus: {
+                    this.score += config.scoreForBonus;
+                    this.context.updateScore();
+
+                    nextCell.state = CellState.cursor;
+                    this.context.updateCell(next);
+
+                    this.growth += config.growthForBonus;
+                    this.cursors.splice(0, 0, next);
+                    this.createBonus();
+                    break;
+                }
+            }
+            if (this.growth != 0) {
+                this.growth--;
+                return;
+            }
+            var last = this.cursors[this.cursors.length - 1];
+            this.cursors.splice(this.cursors.length - 1, 1);
+            this.getCell(last).state = CellState.none;
+            this.context.updateCell(last);
+        }
+        private wrapAround(input: Vector): Vector {
+            var y: number = input.y < 0 ? this.dim.y - 1 : input.y >= this.dim.y ? 0 : input.y;
+            var x: number = input.x < 0 ? this.dim.x - 1 : input.x >= this.dim.x ? 0 : input.x;
+            return new Vector(y, x);
+        }
+        turn(dir: Direction) {
+            if (dir == this.direction) {
+                return;
+            }
+            if (this.getCell(this.wrapAround(this.cursors[0].add(directionVectors[dir]))).state == CellState.cursor) {
+                return;
+            }
+            this.direction = dir;
+        }
+    }
+
+    class CellView {
+        private oddSuffix: string;
+        constructor(public cellModel: CellModel, public td: HTMLTableDataCellElement) {
+            this.oddSuffix = (this.cellModel.vec.y + this.cellModel.vec.x) % 2 == 0 ? "0" : "1";
+            var that: CellView = this;
+            this.td.onclick = () => {
+                Snake.ctrl.onClickCell(that.cellModel.vec);
+            };
+            this.td.className = "cell_common cell_" + CellState[this.cellModel.state] + "_" + this.oddSuffix;
+        }
+        refresh() {
+            this.td.className = "cell_common cell_" + CellState[this.cellModel.state] + "_" + this.oddSuffix;
+        }
+    }
+    class View implements IModelContext {
+        private model: Model;
+        private docElem: HTMLDocument;
+        private contentDiv: HTMLDivElement;
+        private board: CellView[][];
+        private scoreDiv: HTMLDivElement;
+        private gameoverDiv: HTMLDivElement;
+        reset(model: Model) {
+            this.model = model;
+            this.docElem = document;
+            this.contentDiv = <HTMLDivElement>this.docElem.getElementById("content");
+            this.scoreDiv = <HTMLDivElement>this.docElem.getElementById("score");
+            this.gameoverDiv = <HTMLDivElement>this.docElem.getElementById("gameover");
             var contentHtml: string = "";
             contentHtml += "<table border='0' cellpadding='0' cellspacing='0'>";
-            for (var row: number = 0; row < height; row++) {
+            for (var y: number = 0; y < this.model.dim.y; y++) {
                 contentHtml += "<tr>";
-                for (var col: number = 0; col < width; col++) {
-                    contentHtml += "<td id=cell_" + row + "_" + col + "></td>";
+                for (var x: number = 0; x < this.model.dim.x; x++) {
+                    contentHtml += "<td id=cell_" + y + "_" + x + "></td>";
                 }
                 contentHtml += "</tr>";
             }
             contentHtml += "</table>";
-            contentDiv.innerHTML = contentHtml;
-            this.cells = [];
-            for (var row: number = 0; row < height; row++) {
-                var line = [];
-                this.cells.push(line);
-                for (var col: number = 0; col < width; col++) {
-                    var td: HTMLTableDataCellElement = <HTMLTableDataCellElement>document.getElementById("cell_" + row + "_" + col);
-                    line.push(new Cell(row, col, td));
+            this.contentDiv.innerHTML = contentHtml;
+            this.board = new Array<CellView[]>(this.model.dim.y);
+            for (var y: number = 0; y < this.model.dim.y; y++) {
+                this.board[y] = new Array<CellView>(this.model.dim.x);
+                for (var x: number = 0; x < this.model.dim.x; x++) {
+                    var td: HTMLTableDataCellElement = <HTMLTableDataCellElement>this.docElem.getElementById("cell_" + y + "_" + x);
+                    this.board[y][x] = new CellView(this.model.board[y][x], td);
                 }
-                contentHtml += "</tr>";
+            }
+            this.scoreDiv.innerHTML = "Score: " + this.model.score;
+            this.gameoverDiv.innerHTML = "";
+        }
+        updateCell(vector: Vector) {
+            if (this.board == null) {
+                return;
+            }
+            this.getCell(vector).refresh();
+        }
+        private getCell(vector: Vector): CellView {
+            return this.board[vector.y][vector.x];
+        }
+        updateScore() {
+            this.scoreDiv.innerHTML = "Score: " + this.model.score;
+        }
+        updateGameover() {
+            if (this.model.gameOver) {
+                this.gameoverDiv.innerHTML = "Game Over!"
             }
         }
     }
-    export module Program {
-        var contentDiv: HTMLDivElement;
-        var width: number = 40;
-        var height: number = 30;
-        var board: Board;
-        export function reset() {
-            contentDiv = <HTMLDivElement>document.getElementById("content");
-            board = new Board(contentDiv, width, height);
-        }
-    }
-    export module Ctrl {
-        import Program = Snake.Program;
-        export function onLoad() {
-            Program.reset();
-        }
-        export function onClickCell(row: number, col: number) {
-            alert("clicked " + row + " " + col);
-        }
-    }
-}
 
-window.onload = () => {
-    Snake.Ctrl.onLoad();
-};
+    enum KeyCode {
+        up = 38,
+        down = 40,
+        left = 37,
+        right = 39,
+        enter = 13,
+    }
+    export class Ctrl {
+        private model: Model;
+        private view: View;
+        private timeoutHandle: number = null;
+        constructor() {
+            var that = this;
+            window.onload = () => { that.onLoad(); };
+        }
+        onLoad() {
+            var that = this;
+            window.onkeydown = (ev: KeyboardEvent) => { that.onKeyDown(ev); };
+            this.onClickRestart();
+        }
+        onClickRestart() {
+            this.model = new Model();
+            this.view = new View();
+            this.model.reset(this.view, new Vector(config.height, config.width));
+            this.view.reset(this.model);
+            this.startTimer();
+        }
+        private startTimer() {
+            this.stopTimer();
+            var that = this;
+            this.timeoutHandle = setTimeout(() => { that.onTimeout(); }, config.timerMillisec);
+        }
+        private stopTimer() {
+            if (this.timeoutHandle == null) {
+                return;
+            }
+            clearTimeout(this.timeoutHandle);
+            this.timeoutHandle = null;
+        }
+        onClickCell(vec: Vector) {
+            var y = vec.y / this.model.dim.y;
+            var x = vec.x / this.model.dim.x;
+            if (y + x < 1) { // left-top
+                if (y - x <= 0) { // top
+                    this.model.turn(Direction.up);
+                } else { // left
+                    this.model.turn(Direction.left);
+                }
+            } else { // right-bottom
+                if (y - x <= 0) { // right
+                    this.model.turn(Direction.right);
+                } else { // bottom
+                    this.model.turn(Direction.down);
+                }
+            }
+        }
+        onTimeout() {
+            if (this.model.gameOver) {
+                return;
+            }
+            this.startTimer();
+            this.model.move();
+        }
+        onKeyDown(ev: KeyboardEvent) {
+            switch (ev.keyCode) {
+                case KeyCode.up: {
+                    this.model.turn(Direction.up);
+                    break;
+                }
+                case KeyCode.down: {
+                    this.model.turn(Direction.down);
+                    break;
+                }
+                case KeyCode.left: {
+                    this.model.turn(Direction.left);
+                    break;
+                }
+                case KeyCode.right: {
+                    this.model.turn(Direction.right);
+                    break;
+                }
+                case KeyCode.enter: {
+                    this.onClickRestart();
+                    break;
+                }
+            }
+        }
+    }
+    export var ctrl = new Ctrl();
+}
