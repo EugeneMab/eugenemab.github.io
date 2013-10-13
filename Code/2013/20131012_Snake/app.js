d78b5299fbe7545cc65f449f77616ed14eb24fce
Eugene Mab <jianhuangysword@msn.com>
2013-10-12 23:09:23 -0700

    Snake save

--- Code/2013/20131012_Snake/app.js
+++ Code/2013/20131012_Snake/app.js
@@ -1,10 +1,54 @@
 var Snake;
 (function (Snake) {
+    var Cell = (function () {
+        function Cell(row, col, td) {
+            this.row = row;
+            this.col = col;
+            this.td = td;
+            var self = this;
+            this.td.onclick = function () {
+                Snake.Ctrl.onClickCell(self.row, self.col);
+            };
+            this.td.className = "cellNone" + ((row + col) % 2);
+        }
+        return Cell;
+    })();
+    var Board = (function () {
+        function Board(contentDiv, width, height) {
+            this.width = width;
+            this.height = height;
+            var contentHtml = "";
+            contentHtml += "<table border='0' cellpadding='0' cellspacing='0'>";
+            for (var row = 0; row < height; row++) {
+                contentHtml += "<tr>";
+                for (var col = 0; col < width; col++) {
+                    contentHtml += "<td id=cell_" + row + "_" + col + "></td>";
+                }
+                contentHtml += "</tr>";
+            }
+            contentHtml += "</table>";
+            contentDiv.innerHTML = contentHtml;
+            this.cells = [];
+            for (var row = 0; row < height; row++) {
+                var line = [];
+                this.cells.push(line);
+                for (var col = 0; col < width; col++) {
+                    var td = document.getElementById("cell_" + row + "_" + col);
+                    line.push(new Cell(row, col, td));
+                }
+                contentHtml += "</tr>";
+            }
+        }
+        return Board;
+    })();
     (function (Program) {
         var contentDiv;
+        var width = 40;
+        var height = 30;
+        var board;
         function reset() {
-            this.contentDiv = document.getElementById("content");
-            this.contentDiv.innerHTML = "Hello";
+            contentDiv = document.getElementById("content");
+            board = new Board(contentDiv, width, height);
         }
         Program.reset = reset;
     })(Snake.Program || (Snake.Program = {}));
@@ -15,6 +59,10 @@ var Snake;
             Program.reset();
         }
         Ctrl.onLoad = onLoad;
+        function onClickCell(row, col) {
+            alert("clicked " + row + " " + col);
+        }
+        Ctrl.onClickCell = onClickCell;
     })(Snake.Ctrl || (Snake.Ctrl = {}));
     var Ctrl = Snake.Ctrl;
 })(Snake || (Snake = {}));
--- Code/2013/20131012_Snake/app.ts
+++ Code/2013/20131012_Snake/app.ts
@@ -1,15 +1,60 @@
 module Snake {
+    class Cell {
+        constructor(public row: number, public col: number, public td: HTMLTableDataCellElement) {
+            var self: Cell = this;
+            this.td.onclick = () => {
+                Snake.Ctrl.onClickCell(self.row, self.col);
+            };
+            this.td.className = "cellNone" + ((row + col) % 2);
+        }
+    }
+    class Board {
+        width: number;
+        height: number;
+        cells: Cell[][];
+        constructor(contentDiv: HTMLDivElement, width: number, height: number) {
+            this.width = width;
+            this.height = height;
+            var contentHtml: string = "";
+            contentHtml += "<table border='0' cellpadding='0' cellspacing='0'>";
+            for (var row: number = 0; row < height; row++) {
+                contentHtml += "<tr>";
+                for (var col: number = 0; col < width; col++) {
+                    contentHtml += "<td id=cell_" + row + "_" + col + "></td>";
+                }
+                contentHtml += "</tr>";
+            }
+            contentHtml += "</table>";
+            contentDiv.innerHTML = contentHtml;
+            this.cells = [];
+            for (var row: number = 0; row < height; row++) {
+                var line = [];
+                this.cells.push(line);
+                for (var col: number = 0; col < width; col++) {
+                    var td: HTMLTableDataCellElement = <HTMLTableDataCellElement>document.getElementById("cell_" + row + "_" + col);
+                    line.push(new Cell(row, col, td));
+                }
+                contentHtml += "</tr>";
+            }
+        }
+    }
     export module Program {
         var contentDiv: HTMLDivElement;
+        var width: number = 40;
+        var height: number = 30;
+        var board: Board;
         export function reset() {
-            this.contentDiv = <HTMLDivElement>document.getElementById("content");
-            this.contentDiv.innerHTML = "Hello";
+            contentDiv = <HTMLDivElement>document.getElementById("content");
+            board = new Board(contentDiv, width, height);
         }
     }
     export module Ctrl {
         import Program = Snake.Program;
         export function onLoad() {
             Program.reset();
+        }
+        export function onClickCell(row: number, col: number) {
+            alert("clicked " + row + " " + col);
         }
     }
 }
