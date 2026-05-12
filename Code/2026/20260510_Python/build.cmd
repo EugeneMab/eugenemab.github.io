42b5ef73d612b749063b14abd4a968b8d514d0ff
EugeneMab <EugeneMab@users.noreply.github.com>
2026-05-12 07:33:33 -0700

    builder excludes test files

--- Code/2026/20260510_Python/build.cmd
+++ Code/2026/20260510_Python/build.cmd
@@ -1,2 +1,6 @@
 @echo off
-src\node_modules\.bin\tsc -p src/tsconfig.json
+call src\node_modules\.bin\tsc -p src/tsconfig.json
+del pub\js\*.test.js >nul 2>nul
+del pub\js\*.test.js.map >nul 2>nul
+del pub\js\vitest.config.js >nul 2>nul
+del pub\js\vitest.config.js.map >nul 2>nul
